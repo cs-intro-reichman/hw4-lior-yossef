2085a1455281019faa938a99416361f90d9f1f68
lior-yossef <lior.yossef@post.runi.ac.il>
2024-11-25 21:05:56 +0200

    handle empty string case

--- MyString.java
+++ MyString.java
@@ -1,5 +1,16 @@
 public class MyString {
     public static void main(String[] args) {
+        boolean test1 = MyString.contains("baba yaga", "baba");
+        boolean test2 = MyString.contains("baba yaga", "");
+        boolean test3 = !MyString.contains("baba yaga", "John Wick is the baba yaga");
+        boolean test4 = !MyString.contains("baba yaga", "Yaga");
+        boolean test5 = !MyString.contains("baba yaga", "babayaga");
+
+        int passed = (test1 ? 1 : 0) + (test2 ? 1 : 0) + (test3 ? 1 : 0) + (test4 ? 1 : 0) +
+                (test5 ? 1 : 0);
+        
+        System.out.println(passed + ": " + test1 + " " + test2 + " " + test3 + " " + test4 + " " + test5);
+
         System.out.println("Testing lowercase:");
         System.out.println("UnHappy : " + lowerCase("UnHappy"));
         System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
@@ -36,6 +47,10 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
+        if (str1 == "" || str2 == "") {
+            return true;
+        }
+        
         for (int i = 0; i <= str1.length() - str2.length(); i++) {
             for (int j = 0; j < str2.length(); j++) {
                 if (str1.charAt(i + j) != str2.charAt(j)) {
