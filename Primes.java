83c7ed7b6e6eeb91bbd57788675c4bd65e9fcc84
lior-yossef <lior.yossef@post.runi.ac.il>
2024-12-01 10:50:53 +0200

    update Primes logic

--- Primes.java
+++ Primes.java
@@ -3,12 +3,27 @@ public class Primes {
         int numLimit = Integer.parseInt(args[0]);
         int primeCount = 0;
         int num = 2;
+        boolean[] isPrimeArr = new boolean[numLimit + 1];
 
         System.out.println("Prime numbers up to " + numLimit + ":");
 
         while (num <= numLimit) {
-            if (isPrime(num)) {
-                primeCount++;
+            isPrimeArr[num] = true;
+            num++;
+        }
+
+        num = 2;
+
+        while (num <= numLimit) {
+            updateIsPrimeArr(isPrimeArr, num);
+            num++;
+        }
+
+        num = 2;
+
+        while (num <= numLimit) {
+            if (isPrimeArr[num]) {
+                primeCount += 1;
                 System.out.println(num);
             }
             num++;
@@ -19,17 +34,11 @@ public class Primes {
         System.out.println("There are " + primeCount + " primes between 2 and " + numLimit + " (" + precent + "% are primes)");               
     }
 
-    public static boolean isPrime(int num) {
-        if (num < 2) {
-            return false;
-        }
-
+    public static void updateIsPrimeArr(boolean[] isPrime, int num) {
         for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                return false;
+            if (isPrime[i] && num % i == 0) {
+                isPrime[num] = false;
             }
         }
-
-        return true;
     }
 }
